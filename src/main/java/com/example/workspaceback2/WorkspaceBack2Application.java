66f1a12d7872b190cc1b0e231a74cfe87727d64e
MarwenAmmi <marwen.ammi@ext.adeo.com>
2023-03-07 11:02:30 +0100

    Initial commit

---
+++ src/main/java/com/example/workspaceback2/WorkspaceBack2Application.java
@@ -0,0 +1,13 @@
+package com.example.workspaceback2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceBack2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkspaceBack2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workspaceback2/WorkspaceBack2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workspaceback2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceBack2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
